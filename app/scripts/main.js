f285deb74758141691ca0a6ad1c909a9f26d0a44
JP <juanpabloacierno@gmail.com>
2016-02-10 23:58:40 -0500

    load data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,37 @@
-console.log('\'Allo \'Allo!');
+$('#edit').click(function(){
+		/*console.log("ok");*/
+	$.getJSON( "data/types.json", function( data ) {
+
+	var items = [];
+	$.each( data, function( key, val ) {
+		items.push( "<li id='" + key + "'>" + val + "</li>" );
+	});
+
+//	console.log(data);
+	$( "<pre/>", {
+		html: JSON.stringify(data)
+	}).appendTo( "#dbpre" );
+	});
+
+
+/*	var jqxhr = $.getJSON( "data/types.json", function() {
+  console.log( "success" );
+})
+  .done(function() {
+    console.log( "second success" );
+  })
+  .fail(function() {
+    console.log( "error" );
+  })
+  .always(function() {
+    console.log( "complete" );
+  });
+ 
+// Perform other work here ...
+ 
+// Set another completion function for the request above
+jqxhr.complete(function() {
+  console.log( "second complete" );
+});*/
+	
+});
