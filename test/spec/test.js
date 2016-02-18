500d02de6a10d3adfea3b459a061ed94b08742ff
juanpabloacierno <juanpabloacierno@gmail.com>
2016-02-18 13:44:16 -0500

    added some tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -2,10 +2,16 @@
   'use strict';
 
   describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
+    describe('testing calcDates()', function () {
 
-      });
+    	it('should return empty object', function () {
+      		expect(calcSowDates({})).to.equal(undefined);
+      	});
+
+		it('should return an object', function () {
+      		expect(calcSowDates({vopb1: "50", tra1: "35", opb1: "30"},'gaga','02/01/2013').to.equal('ba'));
+      	});
+
     });
   });
 })();
